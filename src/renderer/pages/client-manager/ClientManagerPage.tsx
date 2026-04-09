18e633d9a876cba583f171700177203eca50c6b3
AsierFox <asiergonzalez1995@gmail.com>
2026-04-09 18:28:26 +0200

    feat: adds search filters for clients by name and phone
    Improves client management by introducing user-friendly search
    fields for filtering clients by name (including surname) and
    phone number. Removes unused product type data and related
    logic to streamline component code. Updates sort logic and
    table labels for better clarity and maintainability.

--- src/renderer/pages/client-manager/ClientManagerPage.tsx
+++ src/renderer/pages/client-manager/ClientManagerPage.tsx
@@ -1,11 +1,10 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Alert, Button, Form, notification, Space } from 'antd';
+import { Alert, Button, Form, Space } from 'antd';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ClientModel from '../../../main/models/client.model';
-import ProductTypeModel from '../../../main/models/productType.model';
 import { ROUTES } from '../../app/constants';
 import AdminLayout from '../../layouts/AdminLayout';
 import ClientManagerTable from './ClientManagerTable';
@@ -17,78 +16,9 @@ const ClientManagerPage: React.FC = () => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [tableDataSource, setTableDataSource] = useState<ClientModel[]>([]);
   const [clients, setClients] = useState<ClientModel[]>([]);
-  const [productTypes, setProductTypes] = useState<ProductTypeModel[]>([]);
 
-  const [api, contextHolder] = notification.useNotification();
   const navigate = useNavigate();
   const [form] = Form.useForm();
-
-  // const handleFilter = useCallback(
-  //   (filters: Readonly<Record<string, ProductStockFilterValue>>) => {
-  //     const filteredData = products.filter((item) => {
-  //       return Object.values(filters).every(
-  //         (filter: ProductStockFilterValue) => {
-  //           const itemValue = item[filter.targetKey];
-
-  //           switch (filter.type) {
-  //             case 'SINGLE':
-  //               if (!filter.value) {
-  //                 return true;
-  //               }
-  //               return util.includesStrings(itemValue, filter.value);
-
-  //             case 'MULTIPLE':
-  //               if (filter.value.length <= 0) {
-  //                 return true;
-  //               }
-  //               // Formateamos el valor en caso de que llegue a ser un number
-  //               const searchValue = isNaN(itemValue)
-  //                 ? itemValue.toUpperCase()
-  //                 : itemValue;
-
-  //               return filter.value.includes(searchValue);
-
-  //             case 'RANGE_NUMBER':
-  //               const min = filter.value.min ? Number(filter.value.min) : null;
-  //               const max = filter.value.max ? Number(filter.value.max) : null;
-
-  //               if (min != null && max != null) {
-  //                 return Number(itemValue) >= min && Number(itemValue) <= max;
-  //               }
-  //               if (min != null) {
-  //                 return Number(itemValue) >= min;
-  //               }
-  //               if (max != null) {
-  //                 return Number(itemValue) <= max;
-  //               }
-  //               return true;
-
-  //             case 'RANGE_DATE':
-  //               if (!filter.value.min || !filter.value.max) {
-  //                 return true;
-  //               }
-  //               // Si no hay valor en el campo del registro, y el filtro de fecha busca un rango, lo filtramos
-  //               if (!itemValue) {
-  //                 return false;
-  //               }
-
-  //               return dayjs(itemValue).isBetween(
-  //                 dayjs(filter.value.min),
-  //                 dayjs(filter.value.max),
-  //                 'day',
-  //                 '[]',
-  //               );
-  //             default:
-  //               return true;
-  //           }
-  //         },
-  //       );
-  //     });
-
-  //     setTableDataSource(filteredData);
-  //   },
-  //   [products],
-  // );
 
   const handleNewClient = () => {
     navigate(ROUTES.CLIENT_MANAGER_FORM);
@@ -99,23 +29,24 @@ const ClientManagerPage: React.FC = () => {
   };
 
   useEffect(() => {
-    const getProductsWithTypes = async () => {
-      const clientsFetched = await window.electron.ipcMysql.getClients();
-      const productsTypesFetched =
-        await window.electron.ipcMysql.getProductTypes();
-      setClients(clientsFetched);
-      setTableDataSource(clientsFetched);
-      setProductTypes(productsTypesFetched);
-
-      setLoading(false);
+    const getClients = async () => {
+      try {
+        const clientsFetched = await window.electron.ipcMysql.getClients();
+        setClients(clientsFetched);
+        setTableDataSource(clientsFetched);
+      } catch {
+        setErrorMessage('¡Error al obtener los clientes de la Base de Datos!');
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    getProductsWithTypes();
+    getClients();
   }, []);
 
   return (
     <AdminLayout>
-      {contextHolder}
       {errorMessage ? (
         <Alert
           message="Error"
@@ -141,7 +72,6 @@ const ClientManagerPage: React.FC = () => {
           loading={isLoading}
           dataSource={tableDataSource}
           clients={clients}
-          productTypes={productTypes}
           onEdit={handleEdit}
         />
       </Form>
--- src/renderer/pages/client-manager/ClientManagerTable.tsx
+++ src/renderer/pages/client-manager/ClientManagerTable.tsx
@@ -1,18 +1,20 @@
-import { EditOutlined } from '@ant-design/icons';
-import { Button, Space, Table } from 'antd';
-import dayjs from 'dayjs';
+import {
+  EditOutlined,
+  FileSearchOutlined,
+  PhoneFilled,
+} from '@ant-design/icons';
+import { Button, Col, Input, Row, Space, Table, Typography } from 'antd';
 import React, { useMemo } from 'react';
 import ClientModel from '../../../main/models/client.model';
-import ProductModel from '../../../main/models/product.model';
-import ProductTypeModel from '../../../main/models/productType.model';
 import { CLIENT_FIELD_NAMES } from '../../app/constants';
 import utils from '../../utils/util';
+
+const { Text } = Typography;
 
 interface Props {
   loading: boolean;
   dataSource: ClientModel[];
   clients: ClientModel[]; // Para los filtros
-  productTypes: ProductTypeModel[];
   onEdit: (record: ClientModel) => void;
 }
 
@@ -20,18 +22,13 @@ const ClientManagerTable: React.FC<Props> = ({
   loading,
   dataSource,
   clients,
-  productTypes,
   onEdit,
 }) => {
-  const columns: any = useMemo(() => {
-    const proveedorFilters = utils
-      .uniq(
-        clients
-          .filter((c) => c.proveedor)
-          .map((client: ClientModel) => client.proveedor?.toUpperCase()),
-      )
-      .map((filter: string) => ({ text: filter, value: filter }));
+  const [searchFilters, setSearchFilters] = React.useState<
+    Record<string, string>
+  >({});
 
+  const columns: any = useMemo(() => {
     return [
       {
         title: CLIENT_FIELD_NAMES.id,
@@ -40,16 +37,14 @@ const ClientManagerTable: React.FC<Props> = ({
       {
         title: CLIENT_FIELD_NAMES.nombre,
         dataIndex: 'nombre',
-        filters: proveedorFilters,
-        filterSearch: true,
-        onFilter: (value: string, record: ProductModel) =>
-          utils.equalsStrings(record.nombre, value),
-        sorter: (a: ProductModel, b: ProductModel) =>
-          a.proveedor?.localeCompare(b?.nombre),
+        sorter: (a: ClientModel, b: ClientModel) =>
+          a.nombre?.localeCompare(b?.nombre),
       },
       {
         title: CLIENT_FIELD_NAMES.apellidos,
         dataIndex: 'apellidos',
+        sorter: (a: ClientModel, b: ClientModel) =>
+          a.apellidos?.localeCompare(b?.apellidos),
       },
       {
         title: CLIENT_FIELD_NAMES.direccion,
@@ -101,24 +96,93 @@ const ClientManagerTable: React.FC<Props> = ({
       },
     ];
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [clients, productTypes, onEdit]);
+  }, [clients, onEdit]);
+
+  const filteredDataSource = useMemo(() => {
+    return dataSource.filter((item) => {
+      return Object.entries(searchFilters).every(([field, value]) => {
+        switch (field) {
+          case 'nombre': {
+            const fullName = `${item.nombre} ${item.apellidos}`.toLowerCase();
+            if (fullName.includes(value.toLowerCase())) {
+              return true;
+            }
+            return false;
+          }
+          case 'telefono': {
+            if (item.telefono?.includes(value)) {
+              return true;
+            }
+            return false;
+          }
+          default:
+            return false;
+        }
+      });
+    });
+  }, [dataSource, searchFilters]);
 
   return (
-    <Table
-      rowKey="id"
-      loading={loading}
-      columns={columns}
-      dataSource={dataSource}
-      scroll={{ x: 'max-content' }}
-      pagination={{
-        defaultPageSize: 50,
-        showSizeChanger: true,
-        pageSizeOptions: ['10', '20', '50', '100', '500'],
-        position: ['bottomCenter'],
-        showTotal: (total, range) =>
-          `Mostrando ${range[1] - range[0] + 1} de un total de ${total} productos`,
-      }}
-    />
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+      {/* FILTROS */}
+      <Row gutter={[24, 16]}>
+        <Col>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Text strong style={{ marginBottom: '4px', color: '#595959' }}>
+              Búsqueda por Nombre y Apellidos
+            </Text>
+            <Input
+              placeholder="Nombre o Apellidos..."
+              prefix={<FileSearchOutlined style={{ color: '#13c2c2' }} />}
+              onChange={(e) =>
+                setSearchFilters((prev) => ({
+                  ...prev,
+                  nombre: e.target.value,
+                }))
+              }
+              style={{ width: 350 }}
+              allowClear
+            />
+          </div>
+        </Col>
+
+        <Col>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Text strong style={{ marginBottom: '4px', color: '#595959' }}>
+              Búsqueda por Teléfono
+            </Text>
+            <Input
+              placeholder="Teléfono..."
+              prefix={<PhoneFilled style={{ color: '#13c2c2' }} />}
+              onChange={(e) =>
+                setSearchFilters((prev) => ({
+                  ...prev,
+                  telefono: e.target.value,
+                }))
+              }
+              style={{ width: 300 }} // Un poco más estrecho para el teléfono
+              allowClear
+            />
+          </div>
+        </Col>
+      </Row>
+
+      <Table
+        rowKey="id"
+        loading={loading}
+        columns={columns}
+        dataSource={filteredDataSource}
+        scroll={{ x: 'max-content' }}
+        pagination={{
+          defaultPageSize: 50,
+          showSizeChanger: true,
+          pageSizeOptions: ['10', '20', '50', '100', '500'],
+          position: ['bottomCenter'],
+          showTotal: (total, range) =>
+            `Mostrando ${range[1] - range[0] + 1} de un total de ${total} clientes`,
+        }}
+      />
+    </div>
   );
 };
 
